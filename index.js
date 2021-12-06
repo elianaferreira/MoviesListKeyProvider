519b9f7b3ae8e00a078ae9ea3b0fb24c0c5653c1
Eliana Ferreira <elianaef817@gmail.com>
2021-12-06 00:58:47 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const MOVIEDB_API_KEY = process.env.BOT_TOKEN
+
+
+const express = require('express');
+const app = express();
+
+app.get('/api_key', function(req, res) {
+  res.json({
+      apiKey: MOVIEDB_API_KEY
+    });
+});
+  
+// Setting the server to listen at port 3000
+app.listen(3000, function(req, res) {
+  console.log("Server is running at port 3000");
+});
